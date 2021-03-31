d762d5b14a01c1ddf612773f6b3e7f76272469ec
karankamat <41754574+karankamat@users.noreply.github.com>
2021-03-31 10:43:55 +0530

    Converting characters to words

--- Strings/convert_characters_to_words.py
+++ Strings/convert_characters_to_words.py
@@ -3,3 +3,5 @@ sentence = "What is the Airspeed Velocity of an Unladen Swallow"
 words = sentence.split(" ")
 
 print(words)
+
+# Output: ['What', 'is', 'the', 'Airspeed', 'Velocity', 'of', 'an', 'Unladen', 'Swallow']
